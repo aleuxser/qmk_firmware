780d58e9308619055c0bff54420b35061b42793b
aleuxser <aleuxser@gmail.com>
2021-12-25 12:30:28 +0300

    readme: no bs

--- keyboards/annepro2/keymaps/aleuxser/keymap.c
+++ keyboards/annepro2/keymaps/aleuxser/keymap.c
@@ -6,183 +6,93 @@
 #define ______ KC_NO
 
 enum anne_pro_layers {
-  _BASE_LAYER,
-  _ONESHOT_LAYER,
-  _NUM_CTRL_LAYER,
-  _NAVIGATE_LAYER,
-  _MOUSE_LAYER,
+  _BASE_LAYER, // base (en)
+  _RU_BASE_LAYER, // base (ru)
+  _NAVIGATION_LAYER,/* 2, // oneshot */
+  /* 3, // ctrlnum */
+  /* 4, // navigation */
+  /* 5, // mouse */
 };
 
 // unicode snippet
-void matrix_init_user(void) {
-    set_unicode_input_mode(UC_LNX);
+void matrix_init_user(void) { set_unicode_input_mode(UC_LNX); };
+
+// tap dance for Ь and Ъ
+enum {
+	/* RU_SOFT_HARD, */
+	RU_ER_HARD,
+	RU_ZHE_E,
+	RU_HA_SHTI,
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+	/* [RU_SOFT_HARD] = ACTION_TAP_DANCE_DOUBLE(KC_M, KC_RBRC), */
+	[RU_ER_HARD] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_RBRC),
+	[RU_ZHE_E] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_QUOT),
+	[RU_HA_SHTI] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_GRV),
 };
 
-// Key symbols are based on QMK. Use them to remap your keyboard
-/*
-Layer _BASE_LAYER
-,-----------------------------------------------------------------------------------------.
-| esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
-|-----------------------------------------------------------------------------------------+
-| Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
-|-----------------------------------------------------------------------------------------+
-| Caps    |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
-|-----------------------------------------------------------------------------------------+
-| Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
-|-----------------------------------------------------------------------------------------+
-| Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
-\-----------------------------------------------------------------------------------------/
-Layer 0 (_BASE_LAYER)
-,-----------------------------------------------------------------------------------------.
-|     |     |  P  |     |  H  |  Y  |  X  |     |  Z  |  W  |  D  |     |  L  |           |
-|-----------------------------------------------------------------------------------------+
-|        |  Q  |  E  |  A  |  O  |  I  |  U  |  M  |  S  |  T  |  R  |  N  |  C  |        |
-|-----------------------------------------------------------------------------------------+
-|         |     |  J  |-----|  K  |  V  |-----|-----|  G  |  F  |-----|  B  |             |
-|-----------------------------------------------------------------------------------------+
-|            |     |     |TO(1)|  ,  |  .  |CtlBS|space|  /  |     |     |                |
-|-----------------------------------------------------------------------------------------+
-|       | OSctl |       |                                 |       |       | OSsft |       |
-\-----------------------------------------------------------------------------------------/
-*/
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE_LAYER] = KEYMAP( /* Base */
-	______, ______, KC_P, ______, KC_H, KC_Y, KC_X, ______, KC_Z, KC_W, KC_D, ______, KC_L, ______,
-        ______, KC_Q, KC_E, KC_A, MT(MOD_LALT,KC_O), MT(MOD_LGUI,KC_I), KC_U, KC_M, KC_S, KC_T, MT(MOD_RGUI,KC_R), MT(MOD_RALT,KC_N), KC_C, ______,
-        ______, KC_J, ______, KC_K, KC_V, ______, ______, KC_G, KC_F, ______, KC_B, ______,
-        ______, ______, ______, OSL(_ONESHOT_LAYER), KC_COMM, KC_DOT, KC_SLSH, LCTL(KC_BSPC), KC_SPC, ______, ______, ______,
-        ______, MT(MO(_MOUSE_LAYER),OSM(MOD_LCTL)), ______, ______, ______, ______, ______, MT(MO(_MOUSE_LAYER),OSM(MOD_LSFT)), ______
-    /* KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, */
-    /* KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, */
-    /* LT(_FN1_LAYER,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, */
-    /* KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP), */
-    /* KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(_FN1_LAYER,KC_LEFT), LT(_FN2_LAYER,KC_DOWN), RCTL_T(KC_RGHT) */
-),
-  /*
-   * Layer _BASE_LAYER
+[_BASE_LAYER] = KEYMAP(
+/*
 ,-----------------------------------------------------------------------------------------.
-|     |     |  p  |     |  h  |  y  |  x  |  z  |  w  |  d  |     |  l  |  v  |           |
+|     |     |     |  X  |  Y  |  P  |Sft+I|  F  |  G  |  C  |  R  |     |  L  |           |
 |-----------------------------------------------------------------------------------------+
-|        |  \  |  {  |  }  |  |  |Enter|  u  |  m  | Tab |  =  |  r  |  n  |  c  |        |
-|        |     |     |RGUI |RAlt |     |     |     |     | LAlt|LGUI |     |     |        |
+|CtlSpace|  A  |  O  |  E  |  U  |  I  |     |  D  |  H  |  T  |  N  |  S  |     |        |
 |-----------------------------------------------------------------------------------------+
-|         |  j  |     |     |     |  k  |     |  g  |  f  |     |     |  b  |             |
+|         |     |  Q  |  J  |  K  |     |     |  B  |  M  |  W  |  V  |  Z  |             |
 |-----------------------------------------------------------------------------------------+
-|            |     |     |TO(1)|  ,  |  .  | /   |space|CtlBS|     |     |                |
-|            |     |     |     |     |     |     | Meh |     |     |     |                |
+|            |     |TO(1)|  ,  |  .  |     |CtlBS|space|  /  |     |     |                |
 |-----------------------------------------------------------------------------------------+
-| OSctl | OSctl |       |                                 |       |       | OSsft | OSsft |
-\-----------------------------------------------------------------------------------------/
-  *
-  */
- /* [_SYSTEM_LAYER] = KEYMAP( /1* Base *1/ */
- /*    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, */
- /*    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS, */
- /*    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS, */
- /*    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS, */
- /*    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN2_LAYER), KC_TRNS */
-/* ), */
-[_ONESHOT_LAYER] = KEYMAP( /* Base */
-	______, ______, KC_CIRC, ______, KC_AMPR, KC_ASTR, KC_SCLN, KC_COLN, KC_W, KC_D, ______, KC_L, KC_V, ______,
-        ______, KC_BSLS, KC_LCBR, KC_RCBR, KC_PIPE, KC_ENT, ______, KC_M, KC_S, MT(MOD_LALT,KC_T), MT(MOD_LGUI,KC_R), KC_N, KC_C, ______,
-        ______, KC_J, ______, ______, ______, KC_K, ______, KC_G, KC_F, ______, ______, KC_B, ______,
-        ______, ______, ______, TO(_BASE_LAYER), KC_COMM, KC_DOT, KC_SLSH, MT(KC_MEH,KC_SPC), LCTL(KC_BSPC), ______, ______, ______,
-        OSM(MOD_LCTL), OSM(MOD_LCTL), ______, KC_LSFT, ______, ______, OSM(MOD_LSFT), OSM(MOD_LSFT)
-),
-  /*
-  * Layer _FN2_LAYER
-  * ,-----------------------------------------------------------------------------------------.
-  * |  ~  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |    Bksp   |
-  * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
-  * |-----------------------------------------------------------------------------------------+
-  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
-  * |-----------------------------------------------------------------------------------------+
-  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
-  * |-----------------------------------------------------------------------------------------+
-  * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
-  * \-----------------------------------------------------------------------------------------/
-  *
-  */
- /* [_ENVIROMENT_LAYER] = KEYMAP( /1* Base *1/ */
- /*    KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_TRNS, KC_TRNS, */
- /*    MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS, */
- /*    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS, */
- /*    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(_MOUSE_LAYER), KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS, */
- /*    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_TRNS */
-/* ), */
-[_NUM_CTRL_LAYER] = KEYMAP(
-/* Layer _BASE_LAYER
-,-----------------------------------------------------------------------------------------.
-|     |     |  p  |     |  h  |  y  |  x  |  z  |  w  |  d  |     |  l  |  v  |           |
-|-----------------------------------------------------------------------------------------+
-|        |  q  |  e  |  a  |  o  |  i  |  u  |  m  |  s  |  t  |  r  |  n  |  c  |        |
-|        |     |     |RGUI |RAlt |     |     |     |     | LAlt|LGUI |     |     |        |
-|-----------------------------------------------------------------------------------------+
-|         |  j  |     |     |     |  k  |     |  g  |  f  |     |     |  b  |             |
-|-----------------------------------------------------------------------------------------+
-|            |     |     |TO(1)|  ,  |  .  | /   |space|CtlBS|     |     |                |
-|            |     |     |     |     |     |     | Meh |     |     |     |                |
-|-----------------------------------------------------------------------------------------+
-| OSctl | OSctl |       |                                 |       |       | OSsft | OSsft |
+| OSctl |       |       |                                 |       |       | OSsft |       |
 \-----------------------------------------------------------------------------------------/ */
-	______, ______, KC_P, ______, KC_H, KC_Y, KC_X, KC_Z, KC_W, KC_D, ______, KC_L, KC_V, ______,
-        ______, KC_Q, KC_E, MT(MOD_RGUI,KC_A), MT(MOD_RALT,KC_O), KC_I, KC_U, KC_M, KC_S, MT(MOD_LALT,KC_T), MT(MOD_LGUI,KC_R), KC_N, KC_C, ______,
-        ______, KC_J, ______, ______, ______, KC_K, ______, KC_G, KC_F, ______, ______, KC_B, ______,
-        ______, ______, ______, TO(_ONESHOT_LAYER), KC_COMM, KC_DOT, KC_SLSH, MT(KC_MEH,KC_SPC), LCTL(KC_BSPC), ______, ______, ______,
-        OSM(MOD_LCTL), OSM(MOD_LCTL), ______, KC_LSFT, ______, ______, OSM(MOD_LSFT), OSM(MOD_LSFT)
+	______, ______, ______, KC_X, KC_Y, KC_P, LSFT(KC_I), KC_F, KC_G, KC_C, KC_R, ______, KC_L, ______,
+        LCTL(KC_SPC), KC_A, KC_O, KC_E, KC_U, KC_I, ______, KC_D, KC_H, KC_T, KC_N, KC_S, ______, ______,
+        ______, ______, KC_Q, KC_J, KC_K, ______, ______, KC_B, KC_M, KC_W, KC_V, KC_Z, ______,
+        ______, ______, TO(1), KC_COMM, KC_DOT, ______, LCTL(KC_BSPC), KC_SPC, KC_SLSH, ______, ______, ______,
+        MO(_NAVIGATION_LAYER), ______, ______, KC_ENT, ______, ______, OSM(MOD_LSFT), ______
 ),
-[_NAVIGATE_LAYER] = KEYMAP(
-/* Layer _BASE_LAYER
+[_RU_BASE_LAYER] = KEYMAP(
+/*
 ,-----------------------------------------------------------------------------------------.
-|     |     |  p  |     |  h  |  y  |  x  |  z  |  w  |  d  |     |  l  |  v  |           |
+|     |  Й  |     |  Ц  |  У  |  Е  |  Ф  |  Щ  |  Г  |  Т  |  Н  |     |  З  |           |
 |-----------------------------------------------------------------------------------------+
-|        |  q  |  e  |  a  |  o  |  i  |  u  |  m  |  s  |  t  |  r  |  n  |  c  |        |
-|        |     |     |RGUI |RAlt |     |     |     |     | LAlt|LGUI |     |     |        |
+|        |  К  |  М  |  В  |  А  |  П  |     |  Р Ъ|  О  |  Л  |  Д  |  Ж Э|     |        |
 |-----------------------------------------------------------------------------------------+
-|         |  j  |     |     |     |  k  |     |  g  |  f  |     |     |  b  |             |
+|         |  Я  |  Ч  |  С  |  И  |  Ы  |     |  Ш  |  Ь  |  Б  |  Ю  |  Х Ё|             |
 |-----------------------------------------------------------------------------------------+
-|            |     |     |TO(1)|  ,  |  .  | /   |space|CtlBS|     |     |                |
-|            |     |     |     |     |     |     | Meh |     |     |     |                |
+|            |     |TO(1)|  ,  |  .  |     |CtlBS|space|  /  |     |     |                |
 |-----------------------------------------------------------------------------------------+
-| OSctl | OSctl |       |                                 |       |       | OSsft | OSsft |
+| OSctl |       |       |                                 |       |       | OSsft |       |
 \-----------------------------------------------------------------------------------------/ */
-	______, ______, KC_P, ______, KC_H, KC_Y, KC_X, KC_Z, KC_W, KC_D, ______, KC_L, KC_V, ______,
-        ______, KC_Q, KC_E, MT(MOD_RGUI,KC_A), MT(MOD_RALT,KC_O), KC_I, KC_U, KC_M, KC_S, MT(MOD_LALT,KC_T), MT(MOD_LGUI,KC_R), KC_N, KC_C, ______,
-        ______, KC_J, ______, ______, ______, KC_K, ______, KC_G, KC_F, ______, ______, KC_B, ______,
-        ______, ______, ______, TO(_ONESHOT_LAYER), KC_COMM, KC_DOT, KC_SLSH, MT(KC_MEH,KC_SPC), LCTL(KC_BSPC), ______, ______, ______,
-        OSM(MOD_LCTL), OSM(MOD_LCTL), ______, KC_LSFT, ______, ______, OSM(MOD_LSFT), OSM(MOD_LSFT)
+	______, KC_Q, ______, KC_W, KC_E, KC_T, KC_A, KC_O, KC_U, KC_N, KC_Y, ______, KC_P, ______,
+        ______, KC_R, KC_V, KC_D, KC_F, KC_G, ______, TD(RU_ER_HARD), KC_J, KC_K, KC_L, TD(RU_ZHE_E), ______, ______,
+        ______, KC_Z, KC_X, KC_C, KC_B, KC_S, ______, KC_I, KC_M, KC_COMM, KC_DOT, TD(RU_HA_SHTI), ______,
+        ______, ______, TO(0), KC_QUES, KC_SLSH, ______, LCTL(KC_BSPC), KC_SPC, KC_PIPE, ______, ______, ______,
+        OSM(MOD_LCTL), ______, ______, KC_ENT, ______, ______, OSM(MOD_LSFT), ______
 ),
-[_MOUSE_LAYER] = KEYMAP(
-/* Layer _BASE_LAYER
+[_NAVIGATION_LAYER] = KEYMAP(
+/*
 ,-----------------------------------------------------------------------------------------.
-|     |     |  p  |     |  h  |  y  |  x  |  z  |  w  |  d  |     |  l  |  v  |           |
+|     |     |     |  X  |S_Dwn|S_Up |Sft+I|  F  |PgDwn| PgUp|  R  |     |  L  |           |
 |-----------------------------------------------------------------------------------------+
-|        |  q  |  e  |  a  |  o  |  i  |  u  |  m  |  s  |  t  |  r  |  n  |  c  |        |
-|        |     |     |RGUI |RAlt |     |     |     |     | LAlt|LGUI |     |     |        |
+|        |M_Lft|M_Dwn|M_Up |M_Rht|  I  |     |  D  | Left| Down| Up  |Right|     |        |
 |-----------------------------------------------------------------------------------------+
-|         |  j  |     |     |     |  k  |     |  g  |  f  |     |     |  b  |             |
+|         |     |  Q  |  J  |  K  |     |     |  B  |  M  |  W  |  V  |  Z  |             |
 |-----------------------------------------------------------------------------------------+
-|            |     |     |TO(1)|  ,  |  .  | /   |space|CtlBS|     |     |                |
-|            |     |     |     |     |     |     | Meh |     |     |     |                |
+|            |     | M3  | M1  | M2  |     |CtlBS| Ctrl|  /  |     |     |                |
 |-----------------------------------------------------------------------------------------+
-| OSctl | OSctl |       |                                 |       |       | OSsft | OSsft |
+| OSctl |       |       |                                 |       |       | OSsft |       |
 \-----------------------------------------------------------------------------------------/ */
-	______, ______, KC_P, ______, KC_H, KC_Y, KC_X, KC_Z, KC_WH_D, KC_WH_U, ______, ______, ______, ______,
-        ______, KC_Q, KC_E, MT(MOD_RGUI,KC_A), MT(MOD_RALT,KC_O), KC_I, KC_U, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, ______,
-        ______, KC_J, ______, ______, ______, KC_K, ______, KC_G, KC_F, ______, ______, KC_B, ______,
-        ______, ______, ______, TO(_ONESHOT_LAYER), KC_COMM, KC_DOT, KC_SLSH, MT(KC_MEH,KC_SPC), LCTL(KC_BSPC), ______, ______, ______,
-	______, MT(MO(_MOUSE_LAYER),OSM(MOD_LCTL)), ______, ______, ______, ______, MT(MO(_MOUSE_LAYER),OSM(MOD_LSFT)), ______
+	______, ______, ______, KC_X, KC_MS_WH_DOWN, KC_MS_WH_UP, LSFT(KC_I), KC_F, KC_PGDN, KC_PGUP, KC_R, ______, KC_L, ______,
+        ______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_I, ______, KC_D, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, ______, ______,
+        ______, ______, KC_Q, KC_J, KC_K, ______, ______, KC_B, KC_M, KC_W, KC_V, KC_Z, ______,
+        ______, ______, KC_MS_BTN3, KC_MS_BTN1, KC_MS_BTN2, ______, LCTL(KC_BSPC), KC_LCTL, KC_SLSH, ______, ______, ______,
+        MO(_NAVIGATION_LAYER), ______, ______, KC_ENT, ______, ______, OSM(MOD_LSFT), ______
 ),
 };
 
 
 const uint16_t keymaps_size = sizeof(keymaps);
-
-
-/* void matrix_init_user(void) { */
-
-/* } */
 
 void matrix_scan_user(void) {
 }
